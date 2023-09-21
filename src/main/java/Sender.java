4980fbc2741c10c41dae5800feed4eb9520b82f3
MothTrain <aradgnezami@gmail.com>
2023-09-21 21:39:05 +0100

    Sender class docs added

--- src/main/java/Sender.java
+++ src/main/java/Sender.java
@@ -8,33 +8,55 @@ import java.util.concurrent.TimeUnit;
 import LanChatMessages.Message;
 import com.github.cliftonlabs.json_simple.JsonObject;
 public class Sender extends Thread {
-    private final String IPAddress;
-    private final int port;
     private final Socket socket;
     private final OutputStream outputStream;
+    
+    /**
+     * Queue to write messages that are to be sent. Maximum capacity is 1
+     */
     private final LinkedBlockingQueue<Message> messageQueue = new LinkedBlockingQueue<>(1);
     
     
+    /**
+     * Creates socket resources and waits for messages
+     *
+     * @param ipAddress the IP address of the host
+     * @param port the port number of the host
+     * @throws IOException if an IOException occurs
+     */
     public Sender(String ipAddress, int port) throws IOException {
-        IPAddress = ipAddress;
-        this.port = port;
         
-        socket = new Socket(IPAddress, port);
+        socket = new Socket(ipAddress, port);
         outputStream = socket.getOutputStream();
         
         start();
     }
     
+    /**
+     * Adds a message to the {@link Sender#messageQueue} to sent
+     * through the assigned connection
+     *
+     * @param message The message to send
+     * @throws IllegalStateException If the sender queue is full
+     */
     public synchronized void sendMessage(Message message) throws IllegalStateException {
         messageQueue.add(message);
     }
     
-    public synchronized void writeThroughSocket(Message message) throws IOException {
+    /**
+     * @param message The Json to send
+     * @throws IOException If an IOException occurs during {@link OutputStream} writing
+     */
+    private synchronized void writeThroughSocket(Message message) throws IOException {
         String msgStr = message.toJson();
         
         outputStream.write(msgStr.getBytes(StandardCharsets.UTF_8));
     }
     
+    /**
+     * Closes the socket and {@link OutputStream} and interrupts
+     * the sender {@link #run() messageQueue consumer}
+     */
     public void close() {
         interrupt();
         
@@ -51,6 +73,11 @@ public class Sender extends Thread {
         
     }
     
+    /**
+     * The sender thread consumes {@link Message Messages} and writes to
+     * the {@link OutputStream}. It sends a {@link Message.MessageTypes#WATCHDOG_KICK WATCHDOG_KICK}
+     * when no message has been sent for 1 second
+     */
     @Override
     public void run() {
         while (!isInterrupted()) {
