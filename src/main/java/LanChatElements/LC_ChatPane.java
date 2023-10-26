869042e6690e09e7e5eae6737bd14fafa00913d8
MothTrain <aradgnezami@gmail.com>
2023-10-26 15:50:35 +0100

    LC_ChatPane added for displaying messages. Relevant styles and colours added to LC_Constants

---
+++ src/main/java/LanChatElements/LC_ChatPane.java
@@ -0,0 +1,205 @@
+package LanChatElements;
+
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
+import java.io.StringBufferInputStream;
+import java.text.AttributedString;
+import java.util.ArrayList;
+
+public class LC_ChatPane {
+    private final JTextPane textPane = new JTextPane() {{
+        setEditable(false);
+    }};
+    
+    private final JScrollPane scrollPane = new JScrollPane(textPane) {{
+        getVerticalScrollBar().setBlockIncrement(10);
+        getVerticalScrollBar().setUnitIncrement(15);
+    }};
+    
+    StyledDocument doc = textPane.getStyledDocument();
+    
+    
+    public void addSentMessage(String msg) {
+        String username = "You:  ";
+        int lengthAtStart = doc.getLength();
+        
+        try {
+            doc.insertString(lengthAtStart, username, LC_Constants.sendersNameStyle);
+        } catch (BadLocationException e) {
+            throw new RuntimeException(e);
+        }
+        doc.setParagraphAttributes(lengthAtStart, username.length(),
+                LC_Constants.sendersNameStyle, false);
+        
+        lengthAtStart = doc.getLength();
+        
+        try {
+            doc.insertString(lengthAtStart, wrapString(msg,
+                    LC_Constants.sendersStyle,
+                            280) + "\n",
+                    LC_Constants.sendersStyle);
+            
+        } catch (BadLocationException e) {
+            throw new RuntimeException(e);
+        }
+        doc.setParagraphAttributes(lengthAtStart, msg.length(),
+                LC_Constants.sendersStyle, false);
+        
+    }
+    
+    
+    public void addReceivedMessage(String msg, String username) {
+        int lengthAtStart = doc.getLength();
+        username = username + ":  ";
+        
+        try {
+            doc.insertString(lengthAtStart,
+                    username,
+                    LC_Constants.receivedNameStyle);
+            
+        } catch (BadLocationException e) {
+            throw new RuntimeException(e);
+        }
+        doc.setParagraphAttributes(lengthAtStart, username.length(),
+                LC_Constants.receivedNameStyle, false);
+        
+        lengthAtStart = doc.getLength();
+        
+        try {
+            doc.insertString(lengthAtStart,
+                    wrapString(msg, LC_Constants.receivedStyle,
+                            280) + "\n",
+                    LC_Constants.receivedStyle);
+            
+        } catch (BadLocationException e) {
+            throw new RuntimeException(e);
+        }
+        doc.setParagraphAttributes(lengthAtStart, msg.length(),
+                LC_Constants.receivedStyle, false);
+        
+    }
+    
+    
+    public void addInformationMessage(String msg) {
+        int lengthAtStart = doc.getLength();
+        
+        try {
+            doc.insertString(lengthAtStart, wrapString(msg,
+                    LC_Constants.informationStyle,
+                    280) + "\n",
+                    LC_Constants.informationStyle);
+            
+        } catch (BadLocationException e) {
+            throw new RuntimeException(e);
+        }
+        doc.setParagraphAttributes(lengthAtStart, msg.length(),
+                LC_Constants.informationStyle, false);
+        
+    }
+    
+    
+    public JScrollPane getTextPane() {
+        return scrollPane;
+    }
+    
+    
+    private String wrapString(String string, SimpleAttributeSet style, int wrapLength) {
+        String[] words = string.split(" ");
+        Font font = getFont(style);
+        
+        FontMetrics fontMetrics = generateFontMetric(font);
+        
+        int[] wordLengths = new int[words.length];
+        
+        for (int i=0; i<words.length; i++) {
+            wordLengths[i] = fontMetrics.stringWidth(words[i]);
+        }
+        
+        int spaceWidth = fontMetrics.stringWidth(" ");
+        
+        ArrayList<StringBuilder> stringBuilderLines = new ArrayList<>();
+        stringBuilderLines.add(new StringBuilder());
+        
+        for (int i = 0; i < words.length; i++) {
+            if (fontMetrics.stringWidth(stringBuilderLines.get(stringBuilderLines.size()-1)
+                    .toString()) + wordLengths[i] <= wrapLength) {
+                
+                stringBuilderLines.get(stringBuilderLines.size() - 1)
+                        .append(words[i]).append(" ");
+            
+            } else if (wordLengths[i] >= wrapLength) {
+                for (String line : splitLargeString(words[i], fontMetrics, wrapLength)) {
+                    stringBuilderLines.add(new StringBuilder(line));
+                }
+                stringBuilderLines.get(stringBuilderLines.size()-1).append(" ");
+                
+            } else if (fontMetrics.stringWidth(stringBuilderLines.get(stringBuilderLines.size()-1)
+                    .toString()) + wordLengths[i] >= wrapLength) {
+                stringBuilderLines.add(new StringBuilder());
+                
+                stringBuilderLines.get(stringBuilderLines.size() - 1)
+                        .append(words[i]).append(" ");
+                
+            } else {throw new RuntimeException("condition gap");}
+        }
+        
+        String output = "";
+        
+        for (StringBuilder i : stringBuilderLines) {
+            output = output.concat(i.toString() + "\n");
+        }
+        return output;
+    }
+    
+    private FontMetrics generateFontMetric(Font font) {
+        Graphics g = textPane.getGraphics();
+        
+        return g.getFontMetrics();
+    }
+    
+    private static Font getFont(SimpleAttributeSet style) {
+        return new Font(
+                StyleConstants.getFontFamily(style),
+                StyleConstants.isBold(style) ? Font.BOLD : Font.PLAIN,
+                StyleConstants.getFontSize(style)
+        );
+    }
+    
+    private ArrayList<String> splitLargeString(String longString,
+                                               FontMetrics fontMetrics,
+                                               int wrapLength) {
+        
+        StringBuilder line = new StringBuilder();
+        StringBuilder string = new StringBuilder();
+        string.append(longString);
+        
+        
+        while (
+                fontMetrics.stringWidth(line + (string.substring(string.length()-1)))
+                        <= wrapLength &&
+                        !string.isEmpty()) {
+            
+            line.append(string.substring(0,1));
+            
+            string.deleteCharAt(0);
+            
+            if (string.isEmpty()) {
+                break;
+            }
+        }
+        
+        if (string.isEmpty()) {
+            return new ArrayList<>() {{add(line.toString());}};
+        } else {
+            return new ArrayList<>() {{
+                add(line.toString());
+                addAll(splitLargeString(string.toString(), fontMetrics, wrapLength));
+            }};
+        }
+        
+    }
+}
--- src/main/java/LanChatElements/LC_Constants.java
+++ src/main/java/LanChatElements/LC_Constants.java
@@ -1,14 +1,67 @@
 package LanChatElements;
 
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import java.awt.*;
 
-class LC_Constants {
+public class LC_Constants {
     private LC_Constants() {}
     
-    public final static Color green = new Color(0,130,0);
+    public final static Color transparent = new Color(0,0,0,0);
+    public final static Color green = new Color(0,135,0);
     public final static Color buttonGreen = new Color(0,123,0);
     public final static Color pressedButtonGreen = new Color(0,110,0);
-    public final static Color grey = new Color(170,170,170);
+    public final static Color grey = new Color(100,100,100);
+    public final static Color sendersBlue = new Color(0,110,244);
+    public final static Color receiversRed = new Color(255, 66, 28);
     
     public final static Font buttonFont = new Font("Arial", Font.PLAIN, 20);
+    
+    public final static SimpleAttributeSet sendersStyle;
+    
+    static {
+        sendersStyle = new SimpleAttributeSet();
+        StyleConstants.setForeground(sendersStyle, Color.BLACK);
+        StyleConstants.setFontSize(sendersStyle, 12);
+        StyleConstants.setAlignment(sendersStyle, StyleConstants.ALIGN_RIGHT);
+        StyleConstants.setFontFamily(sendersStyle, "Arial");
+    }
+    public final static SimpleAttributeSet sendersNameStyle;
+    static {
+        sendersNameStyle = new SimpleAttributeSet();
+        
+        StyleConstants.setForeground(sendersNameStyle, sendersBlue);
+        StyleConstants.setFontSize(sendersNameStyle, 12);
+        StyleConstants.setAlignment(sendersNameStyle, StyleConstants.ALIGN_RIGHT);
+        StyleConstants.setBold(sendersNameStyle, true);
+        StyleConstants.setFontFamily(sendersNameStyle, "Arial");
+    }
+    
+    public final static SimpleAttributeSet receivedStyle;
+    static {
+        receivedStyle = new SimpleAttributeSet();
+        StyleConstants.setForeground(receivedStyle, Color.BLACK);
+        StyleConstants.setFontSize(receivedStyle, 12);
+        StyleConstants.setAlignment(receivedStyle, StyleConstants.ALIGN_LEFT);
+        StyleConstants.setFontFamily(receivedStyle, "Arial");
+    }
+    
+    public final static SimpleAttributeSet receivedNameStyle;
+    static {
+        receivedNameStyle = new SimpleAttributeSet();
+        StyleConstants.setForeground(receivedNameStyle, receiversRed);
+        StyleConstants.setFontSize(receivedNameStyle, 12);
+        StyleConstants.setAlignment(receivedNameStyle, StyleConstants.ALIGN_LEFT);
+        StyleConstants.setBold(receivedNameStyle, true);
+        StyleConstants.setFontFamily(receivedNameStyle, "Arial");
+    }
+    
+    public final static SimpleAttributeSet informationStyle;
+    static {
+        informationStyle = new SimpleAttributeSet();
+        StyleConstants.setForeground(informationStyle, grey);
+        StyleConstants.setFontSize(informationStyle, 10);
+        StyleConstants.setAlignment(informationStyle, StyleConstants.ALIGN_CENTER);
+        StyleConstants.setFontFamily(informationStyle, "Arial");
+    }
 }
