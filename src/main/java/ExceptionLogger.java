016b890eec783b0e313c5f6668cd60015bc604d1
MothTrain <aradgnezami@gmail.com>
2023-11-18 18:14:00 +0000

    Removed test main in ExceptionLogger

--- src/main/java/ExceptionLogger.java
+++ src/main/java/ExceptionLogger.java
@@ -9,9 +9,6 @@ public class ExceptionLogger {
     private final static Logger logger = LogManager.getLogger(ExceptionLogger.class);
     private final static Random r = new Random();
     
-    public static void main(String[] args) {
-        ExceptionLogger.log(Level.ERROR, "s",  new NullPointerException("eeeeeee"));
-    }
     
     public static int log(Level level, Exception e) {
         int code = r.nextInt(10000);
